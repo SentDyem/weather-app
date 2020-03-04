82aa9d8b60c481d69564eb8d5d140bccae21a6ea
SentDyem <sentdyem@my.com>
2020-03-04 14:30:33 +0300

    create ui template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const api =
+    {
+      key: "2c72cae3d465a6dae1d5911721e4f54c",
+      base: "http://api.openweathermap.org/data/2.5/"
+    }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="info">
+          <main>
+              <h1>React погода</h1>
+          </main>
+          <footer>
+              <h5>Приложение было разработано в 2020 году</h5>
+          </footer>
+      </div>
   );
 }
 
