595cb69f214947026074181962f7c9e56b28336c
SentDyem <sentdyem@my.com>
2020-03-04 19:31:36 +0300

    develop ui fix weather api bugs connect sf display font to application

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
-
-const api =
-    {
-      key: "2c72cae3d465a6dae1d5911721e4f54c",
-      base: "http://api.openweathermap.org/data/2.5/"
-    }
+import './components/Forecast/Forecast.js';
+import Forecast from "./components/Forecast/Forecast";
 
 function App() {
   return (
-      <div className="info">
+      <div className="App">
+          <header>
+              <h1>Моя погода</h1>
+              <div className="weather-icon">
+                  <img src="https://lh3.googleusercontent.com/proxy/3BAKLb_hmRqGa---QiGJ5Lo7hWnPEro6GsDDd68kcoYa-FwRKlbkQeCsPEbl4aQAWQuu4YjAtqhT9FqXjL3pATguiOXYaPSESsixxeAuv9W4qM0eBYAPvHu6ZSFWtD58LeDm8JTBaFypNAJ64kq9VePq25xeGf29uQ" alt=""/>
+              </div>
+          </header>
           <main>
-              <h1>React погода</h1>
+              <Forecast />
           </main>
           <footer>
               <h5>Приложение было разработано в 2020 году</h5>
---
+++ src/components/Forecast/Forecast.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+
+const Forecast = () =>
+{
+    let [responseObj, setResponseObj] = useState({});
+    function getForecast() {
+        fetch("https://community-open-weather-map.p.rapidapi.com/weather?callback=test&id=2172797&units=%2522metric%2522%20or%20%2522imperial%2522&mode=xml%252C%20html&q=London%252C%20uk", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+                "x-rapidapi-key": "917aebba3emshc8230717cf29341p17b21bjsnb638d099d13f"
+            }
+        })
+            .then(response => response.json())
+            .then(response => {setResponseObj(response)
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+    };
+    return (
+        <div>
+            <h2>Узнать погоду</h2>
+            <div>
+                {JSON.stringify(responseObj)}
+            </div>
+            <button onClick={getForecast}>Узнать погоду</button>
+        </div>
+    );
+};
+export default Forecast;
